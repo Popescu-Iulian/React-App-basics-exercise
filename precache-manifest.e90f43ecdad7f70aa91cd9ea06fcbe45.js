0a85e8771cf3c6ccb6c4380f279cce085a0bdda6
Popescu Iulian <popescu.iulian@protonmail.com>
2020-04-16 18:47:13 +0300

    Updates

---
+++ precache-manifest.e90f43ecdad7f70aa91cd9ea06fcbe45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dec4e0bfaa21dc841b2404882728f5c3",
+    "url": "/React-App-basics-exercise/index.html"
+  },
+  {
+    "revision": "54ed090e60524c442d5b",
+    "url": "/React-App-basics-exercise/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "092b9b87143767a065b7",
+    "url": "/React-App-basics-exercise/static/js/2.e1a8ed29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-App-basics-exercise/static/js/2.e1a8ed29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54ed090e60524c442d5b",
+    "url": "/React-App-basics-exercise/static/js/main.3ec663ed.chunk.js"
+  },
+  {
+    "revision": "21f2db997edd9b0b6a6d",
+    "url": "/React-App-basics-exercise/static/js/runtime-main.a9330be3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-App-basics-exercise/static/media/logo.5d5d9eef.svg"
+  }
+]);
